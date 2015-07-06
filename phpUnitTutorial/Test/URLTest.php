2d45e7690dc07b866b6d979912abedc8de6d4eeb
Nilambar Sharma <nilambar@outlook.com>
2015-07-06 18:38:35 +0545

    initial commit

---
+++ phpUnitTutorial/Test/URLTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace phpUnitTutorial\Test;
+
+use phpUnitTutorial\URL;
+
+class URLTest extends \PHPUnit_Framework_TestCase
+{
+    // public function testSluggifyReturnsSluggifiedString()
+    // {
+    //     $originalString = 'This string will be sluggified';
+    //     $expectedResult = 'this-string-will-be-sluggified';
+
+    //     $url = new URL();
+
+    //     $result = $url->sluggify($originalString);
+
+    //     $this->assertEquals($expectedResult, $result);
+    // }
+    public function testAnother()
+    {
+      $test = true;
+      $test = false;
+
+        $this->assertTrue( $test );
+        // $originalString = 'hello world';
+        // $expectedResult = 'hello-world';
+
+        // $url = new URL();
+
+        // $result = $url->sluggify($originalString);
+
+    }
+}
